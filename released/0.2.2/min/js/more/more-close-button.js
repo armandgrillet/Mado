f5ca2289c5add76019088b8f1dd9622abaff0d51
ArmandGrillet <armand.grillet@gmail.com>
2014-03-03 22:25:13 +0100

    Mado 0.2.2 is here.

---
+++ released/0.2.2/min/js/more/more-close-button.js
@@ -0,0 +1,28 @@
+/* The JS to control the close button's styles according to the user's OS. */
+
+/* HTML shortcuts. */
+var head; // The "head" section of the option window.
+var stylesheetLink = document.createElement("link"); // Create a "link" node.
+var windowClose; // The close button.
+
+/*
+* Function.
+*
+* Resume:
+	* determineCloseButton (): choose the button to display for closing the frame.
+*/
+function determineCloseButton () {
+	stylesheetLink.setAttribute("rel", "stylesheet");
+	stylesheetLink.setAttribute("type", "text/css");
+
+	if (navigator.appVersion.indexOf("Mac") != -1) { // If the user is on a Mac, redirect to the Mac frame styles.
+		stylesheetLink.setAttribute("href", "../css/more/more-frame-mac.css");
+		windowClose.setAttribute("class", "cta little-icon-mac-close");
+	}
+	else { // If the user is on another type of computer, redirect to the generic frame styles.
+		stylesheetLink.setAttribute("href", "../css/more/more-frame-windows.css");
+		windowClose.setAttribute("class", "cta little-icon-win-close");
+	}
+
+	head.appendChild(stylesheetLink); // Append the link node to the "head" section.
+}
---
+++ released/0.2.2/min/js/more/qanda-onload.js
@@ -0,0 +1,17 @@
+/* All the things to do when qanda.html is loaded. */
+
+window.onload = function() {
+	/*
+    * Shortcut.
+    */
+    head = document.getElementsByTagName("head")[0]; // The "head" section of the option window.
+    windowClose = document.getElementById("window-close");
+
+    /*
+    * Functions.
+    */
+    determineCloseButton(); // Determine the close button style.
+    $(windowClose).on("click", function() {
+        chrome.app.window.current().close();
+    });
+}
