a889d6d96b8044ea270a2401992fe481a2906f17
Armand Grillet <armand.grillet@gmail.com>
2015-01-20 08:54:37 +0100

    Minified version removed

--- min/js/more/about.js
+++
@@ -1,3 +0,0 @@
-/* Copyright (c) 2014 Armand Grillet.
-See the license in the "About" section for further information. */
-function CloseButtonManager(){this.head=$("head")[0],this.close=$("#window-close"),this.close.on("click",function(){chrome.app.window.current().close()}),this.init()}CloseButtonManager.prototype={constructor:CloseButtonManager,init:function(){var a,b=document.createElement("link");b.setAttribute("rel","stylesheet"),b.setAttribute("type","text/css"),a=navigator.appVersion.indexOf("Mac")>-1?"mac":navigator.appVersion.indexOf("Win")>-1?"windows":"chromeos",b.setAttribute("href","../../css/more/more-frame-"+a+".css"),this.close.attr("class","cta little-icon-"+a.substring(0,3)+"-close"),this.head.appendChild(b)}},window.onload=function(){new CloseButtonManager};
