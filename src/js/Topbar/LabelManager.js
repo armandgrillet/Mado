a5cbe116b58bdfd5cde03a3a25dd1443805dd432
Armand Grillet <armand.grillet@gmail.com>
2015-01-31 19:35:45 +0100

    LabelManager ready for localization.

--- src/js/Topbar/LabelManager.js
+++ src/js/Topbar/LabelManager.js
@@ -1,29 +1,62 @@
 function LabelManager() {
     /* Variables. */
-    this.labels = [["#export", "msgExport"], ["#new", "msgNew"], ["#open", "msgOpen"], ["#print", "msgPrint"], ["#recent-button", "msgRecent"], ["#save", "msgSave"], ["#save-as", "msgSaveAs"]]; // Targets and labels linked.
-    this.bigWidth = 1600;
-    this.lastWidth = this.bigWidth; // Used to remove and add titles only when it is useful.
+    this.bigWidth = undefined; // We can display labels above this width.
+    this.topbarButtons = [["#export", "msgExport"], ["#new", "msgNew"], ["#open", "msgOpen"], ["#print", "msgPrint"], ["#recent-button", "msgRecent"], ["#save", "msgSave"], ["#save-as", "msgSaveAs"]]; // Targets and labels linked.
+    this.lastWidth = undefined; // Used to remove and add titles only when it is useful.
 
     /* Events */
     chrome.app.window.current().onBoundsChanged.addListener($.proxy(function () { this.update(); }, this));
 
     /* Initialization */
-    this.update();
+    this.init();
 }
 
 LabelManager.prototype = {
     constructor: LabelManager,
 
-    update: function() {
-        if (chrome.app.window.current().getBounds().width < this.bigWidth && this.lastWidth >= this.bigWidth) {
-            for (var i = 0; i < this.labels.length; i++) {
-                $(this.labels[i][0]).attr("title", chrome.i18n.getMessage(this.labels[i][1])); // We set the localized title.
-            }
-        } else if (chrome.app.window.current().getBounds().width >= this.bigWidth && this.lastWidth < this.bigWidth) {
-            for (var j = 0; j < this.labels.length; j++) {
-                $(this.labels[j][0]).removeAttr("title"); // Removes the titles because we are displaying them entirely.
-            }
+    /* Display labels in the topbar */
+    displayLabels: function() {
+        $(".nav-label").each(function() {
+            $(this).removeClass("hidden");
+            $(this).removeAttr("title");
+        });
+
+        for (var j = 0; j < this.topbarButtons.length; j++) {
+            $(this.topbarButtons[j][0]).removeAttr("title"); // Removes the titles because we are displaying them entirely.
         }
-        this.lastWidth = chrome.app.window.current().getBounds().width;
+    },
+
+    /* Hide labels in the topbar and add a title to each button */
+    hideLabels: function() {
+        $(".nav-label").each(function() {
+            $(this).addClass("hidden");
+        });
+
+        for (var i = 0; i < this.topbarButtons.length; i++) {
+            $(this.topbarButtons[i][0]).attr("title", chrome.i18n.getMessage(this.topbarButtons[i][1])); // We set the localized title.
+        }
+    },
+
+    init: function() {
+        switch (chrome.i18n.getUILanguage()) {
+            case "fr":
+                this.hideLabels(); // French words are too long thus we're not showing labels, only titles.
+                break;
+            default:
+                this.bigWidth = 1600;
+                this.lastWidth = chrome.app.window.current().outerBounds.width;
+                if (chrome.app.window.current().outerBounds.width < this.bigWidth) {
+                    this.hideLabels();
+                }
+        }
+    },
+
+    update: function() {
+        if (chrome.app.window.current().outerBounds.width < this.bigWidth && this.lastWidth >= this.bigWidth) {
+            this.hideLabels();
+        } else if (chrome.app.window.current().outerBounds.width >= this.bigWidth && this.lastWidth < this.bigWidth) {
+            this.displayLabels();
+        }
+        this.lastWidth = chrome.app.window.current().outerBounds.width;
     }
 };
